4267bb57664f282082b7e72a05e540b1a4d365e8
Rscime <Rachelscime@gmail.com>
2023-07-22 13:54:05 -0400

    Category Routes tested and correct

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -7,7 +7,9 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const allCategories = await Category.findAll();
+    const allCategories = await Category.findAll({
+      include: [{ model:Product, as: 'products' }]
+    });
     res.status(200).json(allCategories);
   } catch (err) {
     res.status(500).json(err);
@@ -19,8 +21,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
   try {
     const selectedCategory = await Category.findByPk(req.params.id, {
-      // JOIN with products
-      include: [{ model: Product, as: 'Products' }]
+      include: [{ model:Product, as: 'products' }]
     });
 
     if (!selectedCategory) {
